3e7ac14d5d88fd3787f353344009d18f7a6ec58c
Javier Juan Uceda <javierjuanuceda18@gmail.com>
2024-10-18 19:34:07 +0200

    Commit 4: ImageTransform and UploadImage function well

--- src/lib/utils/UploadImage.ts
+++ src/lib/utils/UploadImage.ts
@@ -1,21 +1,18 @@
 'use client'
 import $ from 'jquery';
-
+const { PUBLIC_CLOUDINARY_CLOUD_NAME,PUBLIC_CLOUDINARY_API_KEY,UPLOAD_PRESET } = process.env;
 export default function UploadImage(file: File, callback) {
     try {
 
         const form = new FormData();
         form.append("file", file, file.name);
-        const apikey = process.env.PUBLIC_CLOUDINARY_API_KEY ?? "";
-        const cloudName = process.env.PUBLIC_CLOUDINARY_CLOUD_NAME ?? "";
-        const uploadPreset = process.env.UPLOAD_PRESET ?? "";
 
-        form.append("upload_preset", uploadPreset);
+        form.append("upload_preset", UPLOAD_PRESET);
         form.append("public_id", `${file.name}`);
-        form.append("api_key", apikey);
+        form.append("api_key", PUBLIC_CLOUDINARY_API_KEY);
 
-        $.ajax(`https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, {
-            "url": `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`,
+        $.ajax(`https://api.cloudinary.com/v1_1/${PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`, {
+            "url": `https://api.cloudinary.com/v1_1/${PUBLIC_CLOUDINARY_CLOUD_NAME}/image/upload`,
             "method": "POST",
             "timeout": 0,
             "processData": false,
@@ -28,8 +25,6 @@ export default function UploadImage(file: File, callback) {
                 callback(parse)
 
             }
-        }).done(async function (response) {
-
         }).catch((t, u) => {
             console.log(t, u)
             callback(null)
